e14b722133dd2c14dcebd7c5c694638a25fa880b
Niko Novak <novakniko2000@gmail.com>
2023-03-22 20:59:15 +0100

    Pocetno postavljanje frontend-a

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>BrziChesse</title>
+
+    
+    <link rel="stylesheet" href="/index.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css" integrity="sha384-RxqHG2ilm4r6aFRpGmBbGTjsqwfqHOKy1ArsMhHusnRO47jcGqpIQqlQK/kmGy9R" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+</head>
+<body>
+</body>
+</template>
