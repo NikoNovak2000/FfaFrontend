4275f62dea4f4f937c4321d91288643a68be595b
Niko Novak <novakniko2000@gmail.com>
2023-03-25 17:02:06 +0100

    Dodavanje fotografija

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                       <h1>Aplikacija brze hrane</h1>
                       <a class="navbar-brand" href="#">
-                        <img src="../public/images/brzicheese.png.png" height="100" class="d-inline-block align-top" alt=""
+                        <img src="../public/Images/brzicheese.jpg" height="100" class="d-inline-block align-top" alt=""
                           loading="lazy" />
                         </a>
                           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
@@ -35,6 +35,48 @@
               </div>
             </div>
           </nav>
+           <table class="table table-hover"> 
+            <thead> 
+                <th scope="coll">Naziv </th>
+                <th scope="coll">Cijena </th>
+                <th scope="coll">Slika </th>
+                <th scope="coll">Kolicina </th>
+
+            </thead>
+            <tbody>
+                <tr>
+                  <td>Baconburger </td>
+                  <td> 8$ </td>
+                  <td><img src="../public/Images/Burgers/Baconburger.jpg" height="100" class="d-inline-block align-top" alt=""
+                            loading="lazy" /> </td>
+                  <td> 1 </td>
+                </tr>
+
+                <tr>
+                  <td>Kebab </td>
+                  <td> 6$ </td>
+                  <td><img src="../public/Images/Kebab/Kebab.jpg" height="100" class="d-inline-block align-top" alt=""
+                            loading="lazy" /> </td>
+                  <td> 1 </td>
+                </tr>
+
+                <tr>
+                  <td>Pizza Mexicana </td>
+                  <td> 10$ </td>
+                  <td><img src="../public/Images/Pizza/PizzaMexicana.jpg" height="100" class="d-inline-block align-top" alt=""
+                            loading="lazy" /> </td>
+                  <td> 1 </td>
+                </tr>
+
+                <tr>
+                  <td>Fried Calamari </td>
+                  <td> 15$ </td>
+                  <td><img src="../public/Images/Seafood/FriedCalamari.jpg" height="100" class="d-inline-block align-top" alt=""
+                            loading="lazy" /> </td>
+                  <td> 1 </td>
+                </tr>
+              </tbody>
+              </table>
         </div>
       </div>
     </div>
@@ -60,5 +102,3 @@ nav {
   }
 }
 </style>
-
-
